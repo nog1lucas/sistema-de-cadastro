af67380a5005ab6a90bff24e569d5261955316d6
nog1lucas <lnogueira.contato@gmaiil.com>
2023-02-17 19:28:21 -0300

    adicionando um console.log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 
 //mais um console log de teste:
 console.log("O paulo fez esse commit sem voce ver na aula!")
+console.log("Enviando para o GitHub")
